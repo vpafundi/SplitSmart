ae5ce8d0ae21c713ad5bcf493b32068ac9f13302
Vincenzo Pafundi <v.pafundi94@gmail.com>
2024-01-25 10:35:32 +0100

    Upload last version

--- sample/ss_chatbot/chatbot.py
+++ sample/ss_chatbot/chatbot.py
@@ -3,21 +3,23 @@ from langchain.docstore.document import Document
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-from operator import itemgetter
-
+import logging
+from dotenv import load_dotenv
 
 import os
 import shutil
-
+# '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'
 
 
 class SingletonClass:
-
+    
+    load_dotenv()
     _self = None
     _embedding_model = None # Model used for the Embedding
     _vector_db = None # Local vector DB
@@ -27,27 +29,32 @@ class SingletonClass:
     _chain = None # Execution chain
     _remove_local_vector_db_flag = False # Indicates if it's necessary to drop the old Vector DB (if exists)
 
-    def __new__(cls):
+    def __new__(cls, **kwargs):
         if cls._self is None:
             cls._self = super().__new__(cls)            
         return cls._self
 
     def __init__(self, **kwargs):
-        
-        self._embedding_model = HuggingFaceEmbeddings(model_name=kwargs['EMBEDDING_MODEL'])
-        self._vector_db_path = kwargs['VECTOR_DB_PATH']
-        self._llm=OpenAI(temperature=0, openai_api_key=kwargs['OPEN_AI_KEY'])
-        self._remove_local_vector_db_flag = bool(kwargs['REMOVE_OLD_VECTOR_FLAG'])
-        self._prompt_path = kwargs['PROMPT_PATH']
+
+        logging.info("Inizialization object")
+
+        os.environ['OPENAI_API_KEY']=kwargs['OPEN_AI_KEY'] # Set OPEN_AI_KEY as env variable
+        self._embedding_model = HuggingFaceEmbeddings(model_name=kwargs['EMBEDDING_MODEL']) # Set up the name of the embedding model
+        self._vector_db_path = kwargs['VECTOR_DB_PATH'] # Set up the path of the Vector DB
+        self._llm=ChatOpenAI(model=kwargs['OPEN_AI_MODEL']) # Set up a ChatOpenAI object with model selected 
+        self._remove_local_vector_db_flag = bool(kwargs['REMOVE_OLD_VECTOR_FLAG']) # Remove old vectorDB flag
+        self._prompt_path = kwargs['PROMPT_PATH'] # Set up the path of the prompt
 
         # Remove existing local DB vector
+        logging.info("_remove_local_vector_db_flag -> %s", str(self._remove_local_vector_db_flag))
+
         if self._remove_local_vector_db_flag:
-            shutil.rmtree(self._vector_db_path) if os.path.exists(self._vector_db_path) else print("{} path doesn't exist".format(self._vector_db_path))
+            shutil.rmtree(self._vector_db_path) if os.path.exists(self._vector_db_path) else logging.info("{} path doesn't exist".format(self._vector_db_path))
 
         # Create a new folder for a local DB Vector if not exists
-        os.mkdir(self._vector_db_path) if not os.path.exists(self._vector_db_path) else print("{} path already exist".format(self._vector_db_path))
+        os.mkdir(self._vector_db_path) if not os.path.exists(self._vector_db_path) else logging.info("{} path already exist".format(self._vector_db_path))
         
-        print('Initializated')
+        logging.info('Initializated')
 
     def _create_document(cls, 
                          article: str, 
@@ -78,13 +85,20 @@ class SingletonClass:
         : (Document) -> with the structure described above
 
         '''
-
+        
+        doc_name = doc_name.strip()
+        article_number = article.split('\n')[1].strip()
+        article_title = article.split('\n')[2].strip()
+        
+        logging.info("Creating Document for article: {}".format(article_number))
         _ = {
-            "doc_name": doc_name.strip(),
-            "article_number": article.split('\n')[1].strip(),
-            "title": article.split('\n')[2].strip()
+            "doc_name": doc_name,
+            "article_number": article_number,
+            "title": article_title
         }
         
+        del doc_name, article_number, article_title # Clean memory
+
         return Document(page_content=article, metadata=_)
 
     def ingest_document(cls, 
@@ -112,6 +126,7 @@ class SingletonClass:
         documents_formatted = []
         loader = TextLoader(file_path=doc_folder_path + '/' + document_name)
         document_ingested = loader.load() # Load the document
+        logging.info("Document {} ingested".format(document_name))
         articles_list = document_ingested[0].page_content.split('\n\n') # Split the document into several Civil Code articles
         
         # create a Document object for each article in the document loaded
@@ -120,7 +135,7 @@ class SingletonClass:
                                                                  doc_name=document_name),
                                   articles_list
                                   )
-        del loader, document_ingested, articles_list
+        del loader, document_ingested, articles_list # Clean memory
 
         return list(documents_formatted)
     
@@ -130,8 +145,12 @@ class SingletonClass:
         ----------------------
         Retrieve the Chroma local vector DB.
         """
+        logging.info("Retrieving existing Chroma DB path...")
+
         cls._vector_db = Chroma(persist_directory=cls._vector_db_path, 
                                 embedding_function=cls._embedding_model)
+        
+        logging.info("Existing vector DB imported")
 
     def chroma_embedding_documents(cls, 
                             doc_list: list[Document]
@@ -149,17 +168,13 @@ class SingletonClass:
             """
 
             # Embed documents and store them into a local Chroma vector DB 
+            logging.info("Embedding started")
 
             cls._vector_db = Chroma.from_documents(documents=doc_list,       # List of the documents
                                           embedding=cls._embedding_model,    # embedding model
                                           persist_directory=cls._vector_db_path          # path to existing db local directory 
                                           )
-
-    def vector_search(cls, query):
-
-        doc_results = cls._vector_db.similarity_search_with_score(query=query, 
-                                                                  k=cls._K)
-        return list(doc_results)
+            logging.info("Documents embedded and stored at the following path: {}".format(cls._vector_db_path))
 
     def create_multiquery_retriever(cls):
         """
@@ -170,15 +185,16 @@ class SingletonClass:
         - vector_db
         - llm
         """
+        logging.info("Starting retriever creation")
         if cls._retriever is None:
             cls._retriever = MultiQueryRetriever.from_llm(
                 retriever=cls._vector_db.as_retriever(), 
                 llm=cls._llm,
                 include_original=True
                 )
-            print('Retriever created')
+            logging.info('Retriever created')
         else:
-            print('Retriever already exists.')
+            logging.info('Retriever already exists.')
 
     def set_prompt(cls):
         """
@@ -186,10 +202,14 @@ class SingletonClass:
         ---------------------
         This method set up the prompt for the LLM
         """
+        logging.warn("Loading LLM prompt")
+
         with open(cls._prompt_path, "r") as file:
             prompt = file.read()
 
         cls._prompt = PromptTemplate.from_template(str(prompt))
+
+        logging.info("LLM prompt loaded")
 
     def translate_input(self, 
                          input: str
@@ -209,13 +229,14 @@ class SingletonClass:
         : (str) -> String Translated
         """
 
+        logging.warn("Translating the input in English")
         prompt = """
         translate in English the following input:
         {}
         """.format(input)
-
+        logging.warn(input)
         response = self._llm.invoke(prompt)
-
+        logging.info("input translated")
         return response
         
     def create_chain(cls) -> LLMChain:
@@ -234,6 +255,8 @@ class SingletonClass:
         : (LLMChain) -> LLM Chain to invoke
         """
 
+        logging.warn("Starting chain creation")
+
         output_parser = StrOutputParser()
 
         setup_and_retrieval = RunnableParallel(
@@ -244,7 +267,7 @@ class SingletonClass:
         )
 
         chain = setup_and_retrieval | cls._prompt | cls._llm | output_parser
-
+        logging.warn("Chain created")
         return chain
 
     
